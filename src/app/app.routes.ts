b6677b331869290e014eb92dd14cecde1a4d9399
Rishi Kokil <rishikokil@gmail.com>
2024-11-21 12:25:52 +0530

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPageComponent }, // Set the default route here
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'user-dashboard', component: UserDashboardComponent }
+];
